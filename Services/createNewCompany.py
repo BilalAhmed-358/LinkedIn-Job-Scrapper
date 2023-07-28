58cc91804a39dd8c75345abef99c93a055b82cd2
BilalAhmed_358 <ahmedkhanbilal358@gmail.com>
2023-07-29 04:46:42 +0500

    Finally logged in via a webdriver

--- Services/createNewCompany.py
+++ Services/createNewCompany.py
@@ -5,22 +5,53 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
-import time
+from time import sleep
+import streamlit as st
+
+driver = None
 
 
-print("new company create function called!")
-# opts = Options()
-# service = Service(executable_path='./chrome.exe')
-# driver = webdriver.Chrome(options=opts, service=service)
+def read_email_and_password_from_file(filename="credentials.txt"):
+    try:
+        with open(filename, "r") as file:
+            lines = file.readlines()
+
+        # Remove any leading/trailing whitespaces and newline characters
+        email = lines[0]
+        password = lines[1]
+
+        return email, password
+    except FileNotFoundError:
+        print(f"File '{filename}' not found.")
+        return None
+    except IndexError:
+        print(
+            f"File '{filename}' should have at least 2 lines (email and password).")
+        return None
 
 
 def addNewCompany(companyUrl):
-    # print("The url of the company is ", companyUrl)
+    global driver
+
+    if driver is None:
+        opts = Options()
+        service = Service(executable_path='./chrome.exe')
+        driver = webdriver.Chrome(options=opts, service=service)
+        driver.maximize_window()
+
     if (companyUrl[len(companyUrl)-1] == '/'):
         companyUrl += "jobs"
-        print(companyUrl)
-
-
-# driver.get("https://www.linkedin.com")
-
-# time.sleep(25)
+        driver.get("https://www.linkedin.com")
+        # idk why the page doesn't load in the first go that's why have to refresh the page
+        driver.refresh()
+        sleep(3)
+        email, loginpassword = read_email_and_password_from_file()
+        username = driver.find_element(By.ID, 'session_key')
+        username.send_keys(email)
+        sleep(0.5)
+        password = driver.find_element(By.ID, 'session_password')
+        password.send_keys(loginpassword)
+        sign_in_button = driver.find_element(By.XPATH, '//*[@type="submit"]')
+        sign_in_button.click()
+        sleep(10)
+        driver.close()
