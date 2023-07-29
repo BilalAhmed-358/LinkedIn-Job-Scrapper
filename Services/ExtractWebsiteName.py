5e9029bf36d3ca30bbb564324aa82495bc8dbc95
Bilal358 <ahmedkhanbilal358@gmail.com>
2023-07-30 00:48:32 +0500

    wrote a function to extract filename from the url

---
+++ Services/ExtractWebsiteName.py
@@ -0,0 +1,12 @@
+from urllib.parse import urlparse
+
+
+def extract_website_name(url):
+    parsed_url = urlparse(url)
+    path = parsed_url.path
+
+    # Split the path using "/" and get the last element (company name)
+    company_name = path.strip("/").split("/")[-2]
+
+
+    return company_name.capitalize()
