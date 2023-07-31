6920f334e69157511bcb3dbea322af3046324370
BilalAhmed_358 <ahmedkhanbilal358@gmail.com>
2023-07-31 13:00:27 +0500

    Reset system to app to initial state

--- companies/Turingcom.py
+++
@@ -1,9 +0,0 @@
-
-import streamlit as st
-from st_pages import add_page_title
-
-add_page_title(layout="wide")
-
-st.write("This is the page for Turingcom company")
-st.write("The following jobs are available in the company")
-st.write(['JOB: 0, https://www.linkedin.com/jobs/search/?currentJobId=3673081830&f_C=18846814&geoId=92000000&origin=COMPANY_PAGE_JOBS_CLUSTER_EXPANSION&originToLandingJobPostings=3673081830%2C3667912330%2C3668329455%2C3668328594%2C3663427472%2C3636737169%2C3668334135%2C3668334132%2C3666935844&refId=fZm9KLtoBtDDdU5biTLYKw%3D%3D&trackingId=P1Xk%2BBze%2Bw8R80RNIy6MzQ%3D%3D&trk=d_flagship3_company', 'JOB: 1, https://www.linkedin.com/jobs/search/?currentJobId=3667912330&f_C=18846814&geoId=92000000&origin=COMPANY_PAGE_JOBS_CLUSTER_EXPANSION&originToLandingJobPostings=3673081830%2C3667912330%2C3668329455%2C3668328594%2C3663427472%2C3636737169%2C3668334135%2C3668334132%2C3666935844&refId=fZm9KLtoBtDDdU5biTLYKw%3D%3D&trackingId=%2FeRuqVPWWDJ22Voh6y6lMg%3D%3D&trk=d_flagship3_company', 'JOB: 2, https://www.linkedin.com/jobs/search/?currentJobId=3668329455&f_C=18846814&geoId=92000000&origin=COMPANY_PAGE_JOBS_CLUSTER_EXPANSION&originToLandingJobPostings=3673081830%2C3667912330%2C3668329455%2C3668328594%2C3663427472%2C3636737169%2C3668334135%2C3668334132%2C3666935844&refId=fZm9KLtoBtDDdU5biTLYKw%3D%3D&trackingId=VEgBdVF0ArnQHsI%2FY5juLA%3D%3D&trk=d_flagship3_company'])
